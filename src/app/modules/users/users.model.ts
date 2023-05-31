88288a89bc08f2bcf502c1f4252f3b0336939c23
siam-asrar <siam.asrar@fieldnation.com>
2023-05-30 21:32:32 -0500

    setup done

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,18 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+type UserModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser, UserModel>(
+  {
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  // this will set the fields "createdAt" & "updatedAt" by default in Mongo like ObjectId
+  { timestamps: true }
+)
+
+const User = model<IUser, UserModel>('User', userSchema)
+
+export default User
