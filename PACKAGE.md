299aa36959d98c0afe4e25a1a33323923d9945d3
siam-asrar <siam.asrar@fieldnation.com>
2023-05-30 19:47:50 -0500

    lint and prettier

---
+++ PACKAGE.md
@@ -0,0 +1,21 @@
+# eslint
+
+@ npm add --save-dev @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint typescript
+DOC: https://eslint.org/docs/latest/use/configure/configuration-files
+
+- eslintrc
+- eslintignore
+
+# prettier
+
+@ npm i prettier
+
+@ npx prettier --write src/config/index.ts
+
+DOC: https://prettier.io/docs/en/configuration.html
+
+- prettierrc
+
+npm i husky
+
+npm i
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response, urlencoded } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response, urlencoded } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response): void => {
-    res.send("Hello World");
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Connected to the University Management Auth Service')
 })
 
-export default app;
+export default app
